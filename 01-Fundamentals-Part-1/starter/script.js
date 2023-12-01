bb5a38a9a505f7eec18ac528640891f8cabd2567
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-01 17:28:47 +0100

    Upload some exercises about basic JavaScript

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,79 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Vasil');
+console.log(23);
+
+let firstName = 'Vasil';
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);  // boolean
+console.log(typeof 28);
+console.log(typeof 'Vasil');
+
+javascriptIsFun = 'Yes';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year); // undefined
+
+year = 1991;
+console.log(typeof year); // number
+
+console.log(typeof null); // object is not corrected in legacy reasons
+
+
+let age = 30; // declare variable
+age = 31; // that changes from 30 to 31 
+
+const birthYear = 1991; // const can not be changed
+birthYear = 1990; // it should not work
+
+
+// Math operators
+const now = 2023;
+const ageVasil = now - 1991;
+const ageSarah = now - 2018
+console.log(ageVasil, ageSarah);
+
+console.log(ageVasil * 2, ageVasil / 10, 2 ** 3);
+// 2 ** means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Vasil';
+const lastName = 'Rusinov';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageVasil > ageSarah); // result = true
+console.log(ageSarah >= 18); // result is false. She is 5 years old.
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2023;
+const ageVasil = now - 1991;
+const ageSarah = now - 2018
+
+console.log(now - 1991 > now - 2018);
