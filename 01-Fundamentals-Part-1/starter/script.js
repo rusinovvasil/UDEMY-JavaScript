e87dcd3777f6f44deadcde265e7c4944d1179e5f
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-02 12:50:38 +0100

    Challenge N.1
    Exercise N.1 - Compare the BMI (Body Mass Index).

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -77,3 +77,11 @@ const ageVasil = now - 1991;
 const ageSarah = now - 2018
 
 console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; //  
+x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageVasil + ageSarah) / 2
+console.log(ageVasil, ageSarah, averageAge);
