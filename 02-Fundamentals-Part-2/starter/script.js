74eb6047f8e18e62194f2470e0e22324a8cdd85d
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-10 14:07:18 +0100

    JavaScript Fundamentals - Part 2
    Function Declaration vs Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -12,7 +12,6 @@ if (hasDriversLicense) console.log('I can drive :D');
 // const private = 534;
 
 
-*/
 
 
 // Function
@@ -38,3 +37,25 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+
+*/
+
+
+// Function Declaration vs. Expressions
+
+// Function Declaration
+
+function calcAge1(birthYear) {
+    return 2023 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear;
+}
+const age2 = calcAge2(1993);
+
+console.log(age1, age2);
