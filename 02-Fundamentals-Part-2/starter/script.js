4001e697aabedb045f33b55345cb4a169c824fae
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-10 13:50:09 +0100

    JavaScript Fundamentals - Part 2
    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,4 @@
+/*
 'use strict';
 
 let hasDriversLicense = false;
@@ -10,3 +11,30 @@ if (hasDriversLicense) console.log('I can drive :D');
 // const interface = 'Audio';
 // const private = 534;
 
+
+*/
+
+
+// Function
+
+function logger() {
+    console.log('My name is Slim Shady');
+}
+
+// Calling / Running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
